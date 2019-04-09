e50e1df30cd73c44902c5e82ee8967d6baaea58a
Oles Odynets <40524044+olchyk98@users.noreply.github.com>
2019-04-09 10:20:37 +0200

    MS

--- client/src/pages/playdesk/index.js
+++ client/src/pages/playdesk/index.js
@@ -10,12 +10,13 @@ import { connect } from 'react-redux';
 
 import client from '../../apollo';
 import links from '../../links';
+import { constructClassName } from '../../utils';
 
 import LoadingIcon from '../__forall__/loadicon';
 
-const QueueItem = () => (
+const QueueItem = ({ id, front }) => (
     <button className="rn-playdesk-queue-item definp">
-        <span>Something in my life</span>
+        <span>{ front }</span>
     </button>
 );
 
@@ -23,27 +24,38 @@ class Queue extends PureComponent {
     render() {
         return(
             <section className="rn-playdesk-queue">
-                <QueueItem />
-                <QueueItem />
-                <QueueItem />
-                <QueueItem />
-                <QueueItem />
+                {
+                    this.props.queue.map(({ id, fronttext }) => (
+                        <QueueItem
+                            key={ id }
+                            id={ id }
+                            front={ fronttext }
+                        />
+                    ))
+                }
             </section>
         );
     }
 }
 
-class Display extends Component {
+class Display extends PureComponent {
     render() {
         return(
             <section className="rn-playdesk-display">
-                <div className="rn-playdesk-display-card">
+                <div className={constructClassName({
+                    "rn-playdesk-display-card": true,
+                    "rotated": this.props.rotated
+                })}>
                     <div className="rn-playdesk-display-card-side front">
                         <h1 className="rn-playdesk-display-card-text">
-                            When began the first world war?
+                            { this.props.card.fronttext }
                         </h1>
                     </div>
-                    <div className="rn-playdesk-display-card-side back"></div>
+                    <div className="rn-playdesk-display-card-side back">
+                        <h1 className="rn-playdesk-display-card-text">
+                            { this.props.card.backtext }
+                        </h1>
+                    </div>
                 </div>
             </section>
         );
@@ -59,34 +71,40 @@ class Controls extends Component {
                         [
                             {
                                 icon: faFlushed,
-                                color: "#A5B6FA"
+                                color: "#A5B6FA",
+                                onClick: () => this.props.onRate(1)
                             },
                             {
                                 icon: faSurprise,
-                                color: "#DEA5ED"
+                                color: "#DEA5ED",
+                                onClick: () => this.props.onRate(2)
                             },
                             {
                                 icon: faMehRollingEyes,
-                                color: "#FBC1D5"
+                                color: "#FBC1D5",
+                                onClick: () => this.props.onRate(3)
                             },
                             {
                                 icon: faSmileBeam,
-                                color: "#C0E48E"
+                                color: "#C0E48E",
+                                onClick: () => this.props.onRate(4)
                             }
                         ]
                     ) : (
                         [
                             {
-                                icon: faBinoculars
+                                icon: faBinoculars,
+                                onClick: this.props.onRotate
                             }
                         ]
-                    )).map(({ icon, color }, index) => (
+                    )).map(({ icon, color, onClick }, index) => (
                         <button
                             key={ index }
                             style={{
                                 color: color || "black"
                             }}
-                            className="rn-playdesk-controls-item definp">
+                            className="rn-playdesk-controls-item definp"
+                            onClick={ onClick }>
                             <FontAwesomeIcon icon={ icon } />
                         </button>
                     ))
@@ -102,12 +120,21 @@ class Hero extends Component {
 
         this.state = {
             isLoading: true,
-            showAnswer: false
+            showAnswer: false,
+            questions: null,
+            queue: null,
+            currentCard: null
         }
     }
 
     componentDidMount() {
         this.loadCards(this.props.match.params.id);
+    }
+
+    getCurrentCard = () => {
+        const a = this.state.queue,
+              b = this.state.currentCard;
+        return (a && b) ? a.find(({ id }) => id === b) : null;
     }
 
     loadCards = deskID => {
@@ -149,7 +176,10 @@ class Hero extends Component {
                 return;
             }
 
-            this.setState(() => ({ isLoading: false }));
+            this.setState(() => ({
+                isLoading: false,
+                questions: a.cards
+            }), this.nextCard);
         }).catch((err) => {
             this.props.history.push(links["DASHBOARD_PAGE"].absolute);
             castError();
@@ -157,11 +187,80 @@ class Hero extends Component {
         });
     }
 
+    nextCard = () => {
+        if(!this.state.questions) return;
+
+        if(this.state.queue && this.state.currentCard) {
+            this.setState(({ queue: a }) => {
+                const b = a.filter(io => io.id !== this.state.currentCard),
+                      c = b[0].id;
+
+                if(!c) { // desk end |> sq
+                    this.endGame();
+                    return({
+                        isLoading: true
+                    });
+                }
+
+                return({
+                    queue: b,
+                    currentCard: c,
+                    showAnswer: false
+                });
+            });
+        } else { // build queue
+            const a = Array.from(this.state.questions).sort(() => Math.random() - 0.5);
+
+            this.setState(() => ({
+                queue: a,
+                currentCard: a[0].id
+            }));
+        }
+    }
+
+    rateCard = rating => {
+        if(!this.state.showAnswer || !this.state.currentCard) return;
+
+        const infu = (obj, itt) => {
+            const a = [];
+
+            for(let ma = 0; ma < itt; ma++) {
+                a.push(Object.assign({}, obj));
+            }
+
+            this.setState(({ queue: b }) => ({
+                queue: [
+                    ...b,
+                    ...a
+                ]
+            }));
+        }
+
+        switch(rating) {
+            case 4:break; // good // passed
+            case 3: // push one
+                infu(this.getCurrentCard());
+            break;
+            case 2: // push double
+                infu(this.getCurrentCard());
+            break;
+            case 1: // bad // push triple
+                infu(this.getCurrentCard());
+            break;
+        }
+
+        this.nextCard();
+    }
+
+    endGame = () => {
+        alert("GAME END");
+    }
+
     render() {
         return(
             <div className="rn rn-playdesk">
                 {
-                    (!this.state.isLoading) ? (
+                    (!this.state.isLoading && this.state.queue) ? (
                         <>
                             <div className="rn-playdesk-container">
                                 <section className="rn-playdesk-progressbar">
@@ -171,13 +270,24 @@ class Hero extends Component {
                                         }}
                                     />
                                 </section>
-                                <Queue />
+                                <Queue
+                                    queue={ this.state.queue }
+                                />
                             </div>
-                            <Display />
+                            <Display
+                                card={ this.getCurrentCard() }
+                                rotated={ this.state.showAnswer }
+                            />
                             <div className="rn-playdesk-container center">
-                                <span className="rn-playdesk-controls_title">Did you know that?</span>
+                                {
+                                    (this.state.showAnswer) ? (
+                                        <span className="rn-playdesk-controls_title">Did you know that?</span>
+                                    ) : null
+                                }
                                 <Controls
-                                    showAnswer={ true }
+                                    showAnswer={ this.state.showAnswer }
+                                    onRotate={ () => (!this.state.showAnswer) ? this.setState({ showAnswer: true }) : null }
+                                    onRate={ rating => (this.state.showAnswer) ? this.rateCard(rating) : null }
                                 />
                             </div>
                         </>
