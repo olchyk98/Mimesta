3ff71c284556a3be77cb11cb05ce0f5954b4fc48
Oles Odynets <40524044+olchyk98@users.noreply.github.com>
2019-04-11 14:42:50 +0200

    MS

--- server/start.sh
+++ server/start.sh
@@ -1,7 +1,7 @@
 # -.- # Oles Odynets. First bash script (you're adopted)
 
-export FLASK_RUN_PORT=4000 # Flask def port
-export FLASK_ENV=development # Env mode f app
-export FLASK_APP=app.py # Root file f app
-export FLASK_DEBUG=1 # Debg mode f app
-flask run # Stt flask app
+export FLASK_RUN_PORT=4000 # Server default port
+export FLASK_ENV=development # Server Environment mode
+export FLASK_APP=app.py # App Root file
+export FLASK_DEBUG=1 # Server debug mode
+flask run # Run server
